39c61103550ddc2a80ec168f8fb717017edf5c78
Ali-Romero <79331758+Ali-Romero@users.noreply.github.com>
2021-09-18 03:55:44 +0600

    gh-pages

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import '@/assets/styles/fonts.sass'
-import '@/assets/styles/reset.sass'
-import '@/assets/styles/helpers.sass'
-
-createApp(App).mount('#app')
